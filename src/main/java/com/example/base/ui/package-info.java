fb17ab694ef61e85bc1d1e73d49b573b730f9647
ziyodullayevj7 <ziyodullayevj7@gmail.com>
2026-07-10 22:26:45 +0500

    first push

--- src/main/java/com/example/base/ui/package-info.java
+++
@@ -1,4 +0,0 @@
-@NullMarked
-package com.example.base.ui;
-
-import org.jspecify.annotations.NullMarked;
--- src/main/java/com/example/examplefeature/package-info.java
+++
@@ -1,5 +0,0 @@
-@NullMarked
-package com.example.examplefeature;
-// TODO Remove this package once you have added real features
-
-import org.jspecify.annotations.NullMarked;
