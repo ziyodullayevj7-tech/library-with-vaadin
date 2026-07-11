fe810869f923323382c4d2f0759101191bad755d
ziyodullayevj7 <ziyodullayevj7@gmail.com>
2026-07-11 17:49:16 +0500

    getById added to BookView

--- src/main/java/com/example/repository/BookRepository.java
+++ src/main/java/com/example/repository/BookRepository.java
@@ -1,11 +1,16 @@
 package com.example.repository;
 
 import com.example.entity.BookEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.net.http.HttpHeaders;
 import java.util.List;
 import java.util.Optional;
 
 public interface BookRepository extends CrudRepository<BookEntity, Integer> {
     Optional<List<BookEntity>> getAllByVisibleIsTrue(Boolean visible);
+
+    @Query("FROM BookEntity WHERE visible IS TRUE AND id =:id")
+    Optional<BookEntity> getById(Integer id);
 }
